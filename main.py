977b940bd9ef6b017d9f73ec019ef3c6dede012a
nikitka-lyceum <nikitanad2017@mail.ru>
2022-11-06 06:19:26 +0300

    Add file

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import sys
+import ctypes
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from classes.Editor import EditorCode
+from config import pathAppData
+
+myappid = 'mycompany.myproduct.subproduct.version' # arbitrary string
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(pathAppData + "icon/logo.png"))
     codeEditor = EditorCode()
     codeEditor.show()
     sys.exit(app.exec())
